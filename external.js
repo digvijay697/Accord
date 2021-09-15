78e70e5130479de53868bc1eee012296b2b36f7e
digvijay697 <bsolanki.ce@gmail.com>
2021-09-15 12:28:02 +0530

    Practise

---
+++ external.js
@@ -0,0 +1,24 @@
+function js(){
+    document.getElementById('fn').innerHTML="textchange"
+}
+function inner(){
+    document.getElementById('inner').innerHTML=6+6
+}
+function dw(){
+    document.write(6+6);
+}
+
+function jsprn(){
+    window.print() 
+}
+function cal(){
+    let x,y,z;
+    x=2;
+    y=3;
+    z=x + y
+    document.getElementById('cal').innerHTML=z
+}
+function block(){
+    document.getElementById('code').innerHTML = "coede";
+    document.getElementById('block').innerHTML = "block";
+}
